fd9ade62535cf2c00efc58abb3a0fba9faebe5a6
Khaled <loodee1512@gmail.com>
2023-12-03 01:39:57 +0200

    enhanced preprocessing, sequence parsing

--- gsp.py
+++ gsp.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 
 def preprocessing(df):
@@ -13,13 +12,50 @@ def preprocessing(df):
     # drop null
     df.dropna(how='any', inplace=True)
 
-    df.drop_duplicates(inplace=True)
-
     df.reset_index(drop=True, inplace=True)
-
-    # Add checking for vertical or horizontal data + transformation
 
 
 test_df = pd.read_excel('data/test_data.xlsx')
 
 preprocessing(test_df)
+
+
+df_dict = {}
+
+for index in range(len(test_df)):
+
+    non_temporal = ''
+    flag = False
+    itemlist = list(test_df.iloc[index, 1])
+    insert_index = 0
+    item_index = 0
+
+    while item_index < len(itemlist):
+
+        item = itemlist[item_index]
+
+        if item == '(':
+            insert_index = item_index
+            flag = True
+            item = ''
+
+        elif item == ')':
+            flag = False
+            item = ''
+
+            for i in range(insert_index, item_index + 1):
+                itemlist.pop(insert_index)
+
+            itemlist.insert(insert_index,non_temporal)
+            non_temporal = ''
+            item_index = insert_index
+
+        elif flag:
+            non_temporal += item
+            item = ''
+
+        item_index += 1
+
+    df_dict[test_df.iloc[index, 0]] = itemlist
+
+print(df_dict)
